3cff24966b10019e4574cc238ac573bf026daee1
Ahmet Eroğlu <erogluahmet791@gmail.com>
2025-11-13 15:50:40 +0300

    Implement basic UI and connect to OOP logic

--- app/src/main/java/com/example/myecommerceapp/MainActivity.java
+++ app/src/main/java/com/example/myecommerceapp/MainActivity.java
@@ -4,6 +4,8 @@ package com.example.myecommerceapp;
 import android.util.Log;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button addProductButton;
+    private TextView totalPriceTextView;
+    private User currentUser;
+    private ShoppingCart cart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +29,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addProductButton = findViewById(R.id.addProductButton);
+        totalPriceTextView = findViewById(R.id.totalPriceTextView);
+
+        currentUser = new User("Ahmet Eroğlu", "ahmet123", "ahmet@mail.com");
+        cart = currentUser.getUserCard();
+
+        addProductButton.setOnClickListener(v -> {
+
+            cart.addProduct(new Clothing("CLT-002", "Designer Jeans", 1200.0, "L", "Blue"));
+            Log.d("ECommerceTest", "Yeni ürün sepete eklendi!");
+
+            double newTotal = cart.calculateTotalPrice();
+
+            totalPriceTextView.setText("Total: " + String.valueOf(newTotal)+ " TL");
+        });
 
 
-        User currentUser=new User("Ahmet Eroğlu", "ahmet123", "ahmet@mail.com");
-        ShoppingCart cart= currentUser.getUserCard();
 
-        cart.addProduct(new Electronics("ELC-001", "Laptop Pro X", 25000.0, "TechBrand", 24));
-        cart.addProduct(new Clothing("CLT-001", "Basic T-Shirt", 350.0, "M", "White"));
-        cart.addProduct(new Electronics("ELC-002", "Wireless Mouse", 750.0, "TechBrand", 12));
-        double total=cart.calculateTotalPrice();
-        Log.d("ECommerceTest" ,"total is "+total);
+
     }
 }
